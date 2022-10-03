1aaf7203c35889189ecb1c01753cc373b316bd83
emines0 <emin.sefa1@gmail.com>
2022-10-03 21:52:56 +0200

    adding reactivity to the app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {robots} from './robots.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <CardList robots={robots}/>
+    <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
